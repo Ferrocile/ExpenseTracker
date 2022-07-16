ba63e000ff47aa39167d9e61b98503119db07dda
Brian D <bkdatz@gmail.com>
2022-07-16 18:53:49 -0400

    Updated date dropdown to filter properly

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,38 +5,30 @@ import Card from "../UI/Card";
 import "./Expenses.css";
 
 const Expenses = (props) => {
-  const [ selectedDate, setSelectedDate ] =  useState("2022");
+  const [selectedDate, setSelectedDate] = useState("2022");
 
-
-  const selectDateOptionHandler = selectedDateOption => {
+  const selectDateOptionHandler = (selectedDateOption) => {
     setSelectedDate(selectedDateOption);
-    
   };
+  const expenseArr = props.expenses.filter(expense => {
+     return expense.date.getFullYear().toString() === selectedDate
+});
 
   return (
     <div>
       <Card className="expenses">
-      <ExpensesFilter selected={selectedDate} onSelectDateOption={selectDateOptionHandler} />
-        <ExpenseItem
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.expenses[1].title}
-          amount={props.expenses[1].amount}
-          date={props.expenses[1].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-          date={props.expenses[2].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.expenses[3].title}
-          amount={props.expenses[3].amount}
-          date={props.expenses[3].date}
-        ></ExpenseItem>
+        <ExpensesFilter
+          selected={selectedDate}
+          onSelectDateOption={selectDateOptionHandler}
+        />
+        {expenseArr.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );

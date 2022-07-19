4a75cef4bdf9f35b13920c8349abedef08789577
NUANCE\brian.datz <brian.datz@nuance.com>
2022-07-19 12:57:07 -0400

    Added Charts

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ExpensesList from "./ExpensesList";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesChart from "./ExpensesChart";
 import Card from "../UI/Card";
 import "./Expenses.css";
 
@@ -21,6 +22,7 @@ const Expenses = (props) => {
           selected={selectedDate}
           onSelectDateOption={selectDateOptionHandler}
         />
+        <ExpensesChart expenses={expenseArr}/>
         <ExpensesList items={expenseArr} />
       </Card>
     </div>
